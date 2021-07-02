c76b69d022494adc5f99f63a664307fdf823051c
Yukwok <billynyk@gmail.com>
2021-07-02 08:22:43 +0800

    modify comment

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ response = requests.get(url="https://news.ycombinator.com/news")
 
 
 soup = BS(markup=response.text, features="html.parser")
-# class is python key word ....
+# class is python key word
 storylinks = soup.find_all(name="a", class_="storylink")
 
 
